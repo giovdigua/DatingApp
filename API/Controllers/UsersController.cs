5aab1e84ae50b08e221e60244ddae32e7dbfef01
Giovanni Di Guardo <giovdigua@gmail.com>
2024-08-04 18:08:59 +0200

    15. Adding a new API Controller

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext context) : ControllerBase
+{    
+   
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = context.Users.ToList();
+
+        return users;
+    }
+    
+    [HttpGet("{id:int}")]
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        var user = context.Users.Find(id);
+
+        if(user == null)
+        {
+            return NotFound();
+        }
+
+        return user;
+    }
+
+}
