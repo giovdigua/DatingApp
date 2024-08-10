55111aa21d07795dce23dc3192870a1e1be4f655
Giovanni Di Guardo <giovdigua@gmail.com>
2024-08-10 19:07:31 +0200

    41 - finish

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,7 +8,8 @@ namespace API.Controllers;
 
 public class UsersController(DataContext context) : BaseApiController
 {    
-   
+
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
@@ -16,6 +18,7 @@ public class UsersController(DataContext context) : BaseApiController
         return users;
     }
     
+    [Authorize]
     [HttpGet("{id:int}")]
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
